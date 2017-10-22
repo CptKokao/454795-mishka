756de7951ce95e1cf18fedd755428896a95337bc
CptKokao <30856643+CptKokao@users.noreply.github.com>
2017-10-22 12:57:34 +0300

    Исправил travis

--- js/cart-popup.js
+++ js/cart-popup.js
@@ -1,7 +1,7 @@
-var productLink = document.querySelector(".product__link ");
+var productLink = document.querySelector(".product__link");
 var cartPopup = document.querySelector(".cart-popup");
 
 productLink.addEventListener("click", function (evt) {
-	evt.preventDefault();
+evt.preventDefault();
     cartPopup.classList.toggle("visually-hidden");
 });
--- js/popup-index.js
+++ js/popup-index.js
@@ -2,6 +2,6 @@ var navToggle = document.querySelector(".main-nav__toggle");
 var navPopup = document.querySelector(".main-nav__popup");
 
 navToggle.addEventListener("click", function (evt) {
-	evt.preventDefault();
+evt.preventDefault();
     navPopup.classList.toggle("visually-hidden");
 });
